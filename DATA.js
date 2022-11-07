0db652d1173810e07d50ae158f13d243f3282d63
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-07 14:09:18 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "1d04a27f-18ce-41c3-80d5-133c4edd9ab3",
+    "home_feed": [
+        {
+            "id": "57c11e8e-e133-45ba-9597-20a68ad41e6d",
+            "artist_name": "walk",
+            "home_carousel": [
+                {
+                    "id": "d9b057b6-cc0d-4b57-884e-dcafc3d238e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                },
+                {
+                    "id": "ca35da37-f83f-4455-bf71-334e73cc3a81",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                },
+                {
+                    "id": "fc8a5866-062e-4f7e-9b2c-2ce77667a00b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/257.png"
+                },
+                {
+                    "id": "7543fe0e-0a6b-4f32-a87e-f5e6b48f6278",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                },
+                {
+                    "id": "96b6b799-4ed8-4ce3-b85b-8c6e3c3f517c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                }
+            ],
+            "art_caption": "nature",
+            "art_description": "Natural process fall along white. Threat professional range door child development right."
+        },
+        {
+            "id": "f9cd0d0d-2368-4940-a3c8-9eb264a427fa",
+            "artist_name": "group",
+            "home_carousel": [
+                {
+                    "id": "58c3431d-d264-4aea-9e18-4ac23db1da7e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/69.png"
+                },
+                {
+                    "id": "397de6dd-d5b5-4ce1-a64b-974116a55b64",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "7e07680f-c332-492d-af48-508c25bc36c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                },
+                {
+                    "id": "47a6228b-e8b7-4ec2-a82c-267096898a5a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "2eea1c80-1661-4b64-acf9-6117ea5487fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                }
+            ],
+            "art_caption": "same",
+            "art_description": "Life TV shake boy billion general. Offer season yet test political son."
+        },
+        {
+            "id": "02827ba1-424d-4f71-b7cb-6d24d52a26c1",
+            "artist_name": "blue",
+            "home_carousel": [
+                {
+                    "id": "c0616342-dd8c-4eeb-8df6-3ea53fded017",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                },
+                {
+                    "id": "6ce1b733-5c90-4bc4-a514-efcbf8eefb44",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                },
+                {
+                    "id": "f869a7d2-c560-4194-8275-78bc63bf5f80",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                },
+                {
+                    "id": "c3de0ab4-96da-40f8-a459-2d4483fb212a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                },
+                {
+                    "id": "0d1cc833-32a7-4718-a543-a34d99c3e9bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                }
+            ],
+            "art_caption": "positive",
+            "art_description": "Worry among couple kid dinner around tend special. Participant statement win must call. Employee miss various despite your."
+        },
+        {
+            "id": "c99d1734-5694-4af1-8324-47500054e84e",
+            "artist_name": "under",
+            "home_carousel": [
+                {
+                    "id": "21e969bc-6b51-45b3-af57-9da4a89dd6b5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "2320b5c3-4920-4225-8a90-1e5f8f6b4e46",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "7a5843df-0bc1-4543-82f5-3dac3a6266d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png"
+                },
+                {
+                    "id": "3207c2c6-6cec-4513-9a5c-514dd3641528",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "e3b091d8-5d7b-452c-88fe-bc22c00b7ae1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                }
+            ],
+            "art_caption": "money",
+            "art_description": "Western something prepare music. Decide management simply new. Center education anyone power explain behind defense."
+        },
+        {
+            "id": "3a630a11-8fbd-4768-a130-bb0dd5c52fde",
+            "artist_name": "health",
+            "home_carousel": [
+                {
+                    "id": "bc4bd25b-4e5d-4667-a263-f018f5adffc4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "8c318fd8-8115-49da-a225-8225d7f6ee55",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/275.png"
+                },
+                {
+                    "id": "31c4927b-4151-43dc-85a5-27c936a5acb2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/283.png"
+                },
+                {
+                    "id": "82fc384a-f5d0-4271-9ea5-7460a15f975a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png"
+                },
+                {
+                    "id": "440a05b7-4a86-4661-81fd-b6df85d410e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                }
+            ],
+            "art_caption": "physical",
+            "art_description": "Attack fund ago international learn actually. Thank like line east beautiful environmental. Almost wonder end data reach travel fight."
+        }
+    ]
+}
